78cad9d168d10a035e3bac7cfe592f2eed30c7c4
personalGitprofile <shahidulislam.personal1@gmail.com>
2024-11-12 17:17:54 +0600

    almost complete leftside of home page

---
+++ src/componets/LeftSideHome/LeftSIdeNews.jsx
@@ -0,0 +1,43 @@
+
+import baby from '../../assets/1.png'
+import { FaCalendarCheck } from "react-icons/fa";
+const LeftSIdeNews = () => {
+    return (
+        <>
+            <div>
+            <img src={baby} alt="" />
+            <h3 className='font-display font-semibold text-lg py-2 text-justify'>Sets the interval between each scroll movement in milliseconds.</h3>
+            <div className='flex '>
+                <div className='grid grid-cols-2 mt-2 '>
+                <p>Sports</p>
+                <p className='flex items-center gap-2 pl-8 text-gray-500'><FaCalendarCheck />  Jan 4,2024</p>
+               </div>
+            </div>
+            </div>
+            
+            <div>
+            <img src={baby} alt="" />
+            <h3 className='font-display font-semibold text-lgpy-2 text-justify'>Sets the interval between each scroll movement in milliseconds.</h3>
+            <div className='flex '>
+                <div className='grid grid-cols-2 mt-2 '>
+                <p>Sports</p>
+                <p className='flex items-center gap-2 pl-8 text-gray-500'><FaCalendarCheck />  Jan 4,2024</p>
+               </div>
+            </div>
+            </div>
+            
+            <div>
+            <img src={baby} alt="" />
+            <h3 className='font-display font-semibold text-lg py-2 text-justify'>Sets the interval between each scroll movement in milliseconds.</h3>
+            <div className='flex '>
+                <div className='grid grid-cols-2 mt-2 '>
+                <p>Sports</p>
+                <p className='flex items-center gap-2 pl-8 text-gray-500'><FaCalendarCheck />  Jan 4,2024</p>
+               </div>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default LeftSIdeNews;
--- src/componets/LeftSideHome/LeftSideHome.jsx
+++ src/componets/LeftSideHome/LeftSideHome.jsx
@@ -1,8 +1,22 @@
+import { Link, NavLink } from "react-router-dom";
+import LeftSIdeNews from "./LeftSIdeNews";
 
 const LeftSideHome = () => {
     return (
         <div className="p-2">
             <h1 className="font-bold p-5">All Category </h1>
+            <div className="font-semibold  ml-7 p-2">
+               <li className="list-none  py-1 hover:bg-red-600 hover:text-white"> <Link className="px-4">World</Link></li>
+               <li className="list-none  py-1 hover:bg-red-600 hover:text-white"> <Link className="px-4">USA</Link></li>
+               <li className="list-none  py-1 hover:bg-red-600 hover:text-white"> <Link className="px-4">Econmics</Link></li>
+               <li className="list-none  py-1 hover:bg-red-600 hover:text-white"> <Link className="px-4">Education</Link></li>
+               <li className="list-none  py-1 hover:bg-red-600 hover:text-white"> <Link className="px-4">Tech</Link></li>
+               <li className="list-none  py-1 hover:bg-red-600 hover:text-white"> <Link className="px-4">Sports</Link></li>
+        
+            </div>
+            <div className="pt-8">
+<LeftSIdeNews></LeftSIdeNews>
+            </div>
         </div>
     );
 };
