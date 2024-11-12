707a242ced9806c2ad4ced4326fd79028ef4f5f2
personalGitprofile <shahidulislam.personal1@gmail.com>
2024-11-12 22:17:11 +0600

    all affairs

--- src/componets/LeftSideHome/LeftSideHome.jsx
+++ src/componets/LeftSideHome/LeftSideHome.jsx
@@ -7,7 +7,8 @@ const LeftSideHome = () => {
             <div className="">
             <h1 className="font-bold p-5">All Category </h1>
             <div className="font-semibold  ml-7 p-2">
-               <li className="list-none  py-1 hover:bg-red-600 hover:text-white"> <Link className="px-4">World</Link></li>
+               <li className="list-none  py-1 hover:bg-red-600 hover:text-white"> <Link className="px-4">All Affairs</Link></li>
+              
                <li className="list-none  py-1 hover:bg-red-600 hover:text-white"> <Link className="px-4">USA</Link></li>
                <li className="list-none  py-1 hover:bg-red-600 hover:text-white"> <Link className="px-4">Econmics</Link></li>
                <li className="list-none  py-1 hover:bg-red-600 hover:text-white"> <Link className="px-4">Education</Link></li>
