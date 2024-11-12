8f198ca469e1c402f093687c3f3364c16aa54bf8
personalGitprofile <shahidulislam.personal1@gmail.com>
2024-11-12 13:05:33 +0600

    navbar responsive included

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../../assets/logo.png'
 const Header = () => {
     return (
         <div>
-            <img className='size-36 w-80' src={logo} alt="" />
+            <img className='size-36 w-24 h-16 md:h-32 md:w-80 ' src={logo} alt="" />
         </div>
     );
 };
